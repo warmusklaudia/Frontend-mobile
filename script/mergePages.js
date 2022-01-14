4167bbeff9dbd4249cedeb56b8b9159cff7066bd
Umair Bhatti <Umair.bhatti@student.howest.be>
2022-01-14 12:08:07 +0100

    pages merged

--- script/mergePages.js
+++ script/mergePages.js
@@ -1,9 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("test")
-    let test = document.querySelector("js-text")
-    console.log(test)
-    // index.html
-    // volgen.html
-    // hulpo_nderweg.html
-    // onderweg.html
+    console.log("Dom is geloaden")
+
+    // 1. classe toevoegen aan index pagina 
+    // 2. classe opvragen
+    // 3. evenlistener eraan koppellen
+    // 4. 
+    // als in de js-index-page bestaat dan voer de volgende code uit
+
+    const params = new URLSearchParams(window.location.search)
+    pagina = params.get("pagina")
+
+    console.log(pagina)
+    if (pagina == "volgen")
+    {
+        console.log("works")
+        // onderstaande vervangt de text in index.html
+        document.querySelector(".js-text").textContent = 'Volg Temi...';
+
+        // onderstaande tijdelijk om te testen
+        function volgendePagina() {
+            window.location.replace("gearriveerd.html");
+        }
+        setTimeout(volgendePagina, 2000);
+    } else if (pagina == "onderweg")
+    {
+        document.querySelector(".js-text").textContent = 'Roep Temi!';
+
+        function volgendePagina() {
+            window.location.replace("locaties.html");
+        }
+        setTimeout(volgendePagina, 2000);
+    } else if (pagina == "help_onderweg")
+    {
+        console.log("test 2520")
+        document.querySelector(".js-text").textContent = 'Er komt zo dadelijk iemand van het onthaal bij u...';
+
+        // function volgendePagina() {
+        //     window.location.replace("index.html/pagina=help_onderweg");
+        //     console.log("testing which page this displays on")
+        // }
+        // setTimeout(volgendePagina, 2000);
+    }
 })
