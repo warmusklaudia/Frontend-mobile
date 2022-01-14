6709d2d934701851b51a66db1c22548dedf6738e
KlaudiaWs <klaudia.warmus@student.howest.be>
2022-01-14 15:45:58 +0100

    id ophalen via db en qr code genereren

--- script/qr.js
+++ script/qr.js
@@ -1,7 +1,28 @@
 // code = 'c78be457-1e1c-49ec-9271-4dc05b3da335';
-var arrayCode = ['c78be457-1e1c-49ec-9271-4dc05b3da335', 'aa4fddf5-fe6f-4036-ae87-c16f4439d81c', '451c5279-f4ff-4b27-ab6c-d1d13044a67a', 'af1b5fdd-3293-4f4c-bb38-a1890c882512', 'a03917bc-811d-42a4-9796-5ff4eeb8940c', '123456789']
+// var arrayCode = ['c78be457-1e1c-49ec-9271-4dc05b3da335', 'aa4fddf5-fe6f-4036-ae87-c16f4439d81c', '451c5279-f4ff-4b27-ab6c-d1d13044a67a', 'af1b5fdd-3293-4f4c-bb38-a1890c882512', 'a03917bc-811d-42a4-9796-5ff4eeb8940c', '123456789'];
 
-var randomCode = arrayCode[Math.floor(Math.random()*arrayCode.length)];
+let afspraakJson = [];
+
+var randomCode = afspraakJson[Math.floor(Math.random() * afspraakJson.length)];
+
+const showResult = (queryResponse) => {
+  for (let i = 0; i < queryResponse.length; i++) {
+    afspraakJson[i] += `${queryResponse[i].afspraakId}`;
+    console.log(afspraakJson[i].substring(9));
+  }
+  console.log(afspraakJson);
+  var randomCode = afspraakJson[Math.floor(Math.random() * afspraakJson.length)];
+  qrcode(randomCode.substring(9));
+};
+
+const get = (url) => fetch(url).then((r) => r.json());
+
+const getAfspraken = async () => {
+  const endPoint = `https://bezoekersapi.azurewebsites.net/api/afspraken`;
+  const response = await get(endPoint);
+  console.log({ response });
+  showResult(response);
+};
 
 const qrcode = (code) => {
   console.log(code);
@@ -16,29 +37,29 @@ const qrcode = (code) => {
   });
 };
 
-if (randomCode == "123456789"){
-  console.log("wrong ID")
+if (randomCode == '123456789') {
+  console.log('wrong ID');
 
-  let pagina = document.getElementById("pagina")
-  pagina.classList.add("js-temp-click")
+  let pagina = document.getElementById('pagina');
+  pagina.classList.add('js-temp-click');
 
-  let nextpage = document.querySelector(".js-temp-click")
-  nextpage.addEventListener("click", function() {
-    window.location.href = "error.html";
-  })  
+  let nextpage = document.querySelector('.js-temp-click');
+  nextpage.addEventListener('click', function () {
+    window.location.href = 'error.html';
+  });
 } else {
-  console.log("Good ID") 
-  
-  let pagina = document.getElementById("pagina")
-  pagina.classList.add("js-temp-click")
+  console.log('Good ID');
 
-  let nextpage = document.querySelector(".js-temp-click")
-  nextpage.addEventListener("click", function() {
-    window.location.href = "omkleden.html";
-  })  
+  let pagina = document.getElementById('pagina');
+  pagina.classList.add('js-temp-click');
+
+  let nextpage = document.querySelector('.js-temp-click');
+  nextpage.addEventListener('click', function () {
+    window.location.href = 'omkleden.html';
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   console.log('DOM geladen');
-  qrcode(randomCode);
+  getAfspraken();
 });
