4a41ebfaae3c1488cd139ffceb0a901d032bc713
KlaudiaWs <klaudia.warmus@student.howest.be>
2022-01-19 15:48:30 +0100

    btn disable

--- script/terugroepen.js
+++ script/terugroepen.js
@@ -1,4 +1,5 @@
-let btnTerugroepen, btnHulp, afspraakId;
+let btnTerugroepen, btnHulp, afspraakId, htmlMess;
+let locatieTemi = 'Home base';
 
 const listenToButtons = () => {
   btnTerugroepen.addEventListener('click', () => {
@@ -9,12 +10,22 @@ const listenToButtons = () => {
   });
 };
 
+const checkLocation = () => {
+  if (locatieTemi == 'Home base') {
+    btnTerugroepen.disabled = false;
+  } else {
+    btnTerugroepen.disabled = true;
+    htmlMess.innerHTML = `Even geduld. Temi is bezig.`;
+  }
+};
+
 document.addEventListener('DOMContentLoaded', function () {
   console.log('DOMContent loaded');
   btnTerugroepen = document.querySelector('.js-btn-roep');
   btnHulp = document.querySelector('.js-help');
-
+  htmlMess = document.querySelector('.js-message');
   const params = new URLSearchParams(window.location.search);
   afspraakId = params.get('afspraakId');
+  checkLocation();
   listenToButtons();
 });
