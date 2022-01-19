7302663e482a3cffeb9276325d5d62433fd5e8ad
KlaudiaWs <klaudia.warmus@student.howest.be>
2022-01-19 13:56:20 +0100

    id-opslaan

--- script/omkleden.js
+++ script/omkleden.js
@@ -1,15 +1,6 @@
 let afspraakId;
 let btnKleedkamer, btnSportscube;
 
-const listenToButton = () => {
-  btnJa.addEventListener('click', () => {
-    window.location.href = `index.html?pagina=volgen&afspraakId=${afspraakId}`;
-  });
-  btnNee.addEventListener('click', () => {
-    window.location.href = `index.html?pagina=volgen&afspraakId=${afspraakId}`;
-  });
-};
-
 const options = {
   keepalive: 60,
   clean: true,
@@ -21,9 +12,11 @@ const client = mqtt.connect('mqtt://13.81.105.139', options);
 
 function listenToButtons() {
   btnKleedkamer.addEventListener('click', function () {
+    window.location.href = `index.html?pagina=volgen&afspraakId=${afspraakId}`;
     client.publish('F2B/locatie', JSON.stringify({ locatie: 'onderweg naar kleedkamer' }));
   });
   btnSportscube.addEventListener('click', function () {
+    window.location.href = `index.html?pagina=volgen&afspraakId=${afspraakId}`;
     client.publish('F2B/locatie', JSON.stringify({ locatie: 'onderweg naar sportscube' }));
   });
 }
@@ -34,7 +27,8 @@ document.addEventListener('DOMContentLoaded', function () {
   btnKleedkamer = document.querySelector('.js-kleedkamer');
   btnSportscube = document.querySelector('.js-sportscube');
 
-  init();
+  const params = new URLSearchParams(window.location.search);
+  afspraakId = params.get('afspraakId');
+
   listenToButtons();
-  listenToButton();
 });
