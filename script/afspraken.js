c68f08a85cc940333e2dee554827707ad0cca735
KlaudiaWs <klaudia.warmus@student.howest.be>
2022-01-28 15:50:01 +0100

    navigation

--- script/afspraken.js
+++ script/afspraken.js
@@ -1,6 +1,6 @@
 'use strict';
 let currentAfspraakID, messageAfspraak;
-let htmlAfspraak;
+let htmlAfspraak, btnAdd, btnHome;
 
 const showAfspraken = function (jsonObject) {
   //Toon menu
@@ -13,7 +13,7 @@ const showAfspraken = function (jsonObject) {
     </div>
     <div class="c-afspraak__date">${afspraak.datum}</div>
     <div class="c-afspraak__hour">${afspraak.tijdstip}</div>
-    <div class="c-afpraak__delete">
+    <div class="c-afspraak__delete">
       <svg class="c-afspraak__delete-symbol js-verwijder-afspraak" data-afspraak-id=${afspraak.afspraakId} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="arcs">
         <polyline points="3 6 5 6 21 6"></polyline>
         <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
@@ -80,13 +80,26 @@ const listenToClickRemoveAfspraak = function () {
   // }, 1000);
 };
 
+const listenToNavigation = () => {
+  btnAdd.addEventListener('click', () => {
+    window.location.href = `afspraak.html`;
+  });
+
+  btnHome.addEventListener('click', () => {
+    window.location.href = `index_admin.html`;
+  });
+};
+
 const init = function () {
   htmlAfspraak = document.querySelector('.js-afspraken');
   messageAfspraak = document.querySelector('.js-mess-verwijderd');
+  btnAdd = document.querySelector('.js-button-add');
+  btnHome = document.querySelector('.js-button-home');
   getAfspraken();
 };
 
 document.addEventListener('DOMContentLoaded', function () {
   console.log('DOM geladen');
   init();
+  listenToNavigation();
 });
